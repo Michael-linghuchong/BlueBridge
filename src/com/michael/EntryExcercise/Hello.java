a8209698ae2eb55526ce085c6b357a7d13b967a9
Michael <947387616@qq.com>
2020-06-28 21:59:45 +0800

    hello word !

---
+++ src/com/michael/EntryExcercise/Hello.java
@@ -0,0 +1,12 @@
+package com.michael.EntryExcercise;
+
+/**
+ * @author Michael-luo
+ * @create 2020-06-28 21:58
+ * @description
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
